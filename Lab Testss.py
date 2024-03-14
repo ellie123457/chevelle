5fc8c1a947fbfba89199a13fd400d0a14435410f
ellie123457 <163459242+ellie123457@users.noreply.github.com>
2024-03-14 14:32:23 -0500

    Add files via upload

---
+++ Lab Testss.py
@@ -0,0 +1,54 @@
+def is_year(Year):
+    if Year < 1752:
+        print ("There are no leap year before 1750")
+    elif Year % 4==0:
+        print ("Leap Year")
+    elif Year % 400==0:
+        print("Leap year")
+    elif Year % 100 !=0:
+        print ("No leap year")
+#main program
+Year = int(input("Please enter the year: "))
+is_year(Year)
+print()
+
+def is_year(Year):
+    if Year < 1752:
+        print ("There are no leap year before 1750")
+    elif Year % 4==0:
+        print ("Leap Year")
+    elif Year % 400==0:
+        print("Leap year")
+    elif Year % 100 !=0:
+        print ("No leap year")
+#main program
+Year = int(input("Please enter the year: "))
+is_year(Year)
+print()
+
+def is_year(Year):
+    if Year < 1752:
+        print ("There are no leap year before 1750")
+    elif Year % 4==0:
+        print ("Leap Year")
+    elif Year % 400==0:
+        print("Leap year")
+    elif Year % 100 !=0:
+        print ("No leap year")
+#main program
+Year = int(input("Please enter the year: "))
+is_year(Year)
+print()
+
+def is_year(Year):
+    if Year < 1752:
+        print ("There are no leap year before 1750")
+    elif Year % 4==0:
+        print ("Leap Year")
+    elif Year % 400==0:
+        print("Leap year")
+    elif Year % 100 !=0:
+        print ("No leap year")
+#main program
+Year = int(input("Please enter the year: "))
+is_year(Year)
